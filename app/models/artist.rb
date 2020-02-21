0b1bb0fc36af080c7b409efa6390925df09de838
Denzhel Distor <denzhelroyceian@gmail.com>
2020-02-21 00:57:24 -0600

    Added validations for bands and artists

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,4 +4,7 @@ class Artist < ApplicationRecord
   has_many :albums
   has_many :members
   has_many :bands, through: :members
+
+  validates :name, presence: true, uniqueness: true
+  validates :info, presence: true, length: { minimum: 20 }
 end
--- app/models/band.rb
+++ app/models/band.rb
@@ -4,4 +4,7 @@ class Band < ApplicationRecord
   has_many :albums
   has_many :members
   has_many :artists, through: :members
+
+  validates :name, presence: true, uniqueness: true
+  validates :info, presence: true, length: { minimum: 20 }
 end
