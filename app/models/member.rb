fa83a747e72e91348656ac71e21e7a184e964f6c
Denzhel Distor <denzhelroyceian@gmail.com>
2020-02-21 00:58:38 -0600

    Added validations for members

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 class Member < ApplicationRecord
   belongs_to :artist
   belongs_to :band
+
+  validates :artist, presence: true
+  validates :band, presence: true
 end
